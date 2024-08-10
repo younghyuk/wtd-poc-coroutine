ea094cb72a7bf7e1a037a913f3f106ca00822bd0
Ethan Yoo <yyhyoo0723@naver.com>
2024-08-10 17:55:12 +0900

    hands on #2-template

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ package com.ethan
 
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 data class User(val name: String)
@@ -12,10 +13,12 @@ class ApiClient {
         delay(1000)
         return User("ethan")
     }
+
     suspend fun loadProduct(): Product {
         delay(500)
-        return Product("macbook")
+        throw IllegalStateException("Network is not working")
     }
+
     suspend fun orderProduct(user: User, product: Product): String {
         delay(1000)
         return "${user.name} / ${product.name}";
@@ -27,11 +30,13 @@ val api = ApiClient()
 fun main() = runBlocking {
     println("Started")
 
-    // Hand on #1-2. User와 Product를 동시에 불러오고 결과로 Product 주문하기
-    val user = async { api.loadUser() }
-    val product = async { api.loadProduct() }
-    val order = api.orderProduct(user.await(), product.await())
-    println("order: $order")
+    // Hand on #2. 에러 처리
+    launch {
+        val user = api.loadUser()
+        val product = api.loadProduct()
+        val order = api.orderProduct(user, product)
+        println("order: $order")
+    }
 
     println("finished")
 }
