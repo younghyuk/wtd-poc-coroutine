fb90195c41d42c7be98e609ebc0961eaaa250745
Ethan Yoo <yyhyoo0723@naver.com>
2024-08-10 17:29:04 +0900

    init template

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,32 @@
 package com.ethan
 
-fun main() {
-    println("Hello World!")
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+data class User(val name: String)
+data class Product(val name: String)
+
+class ApiClient {
+    suspend fun loadUser(): User {
+        delay(1000)
+        return User("ethan")
+    }
+    suspend fun loadProduct(): Product {
+        delay(500)
+        return Product("macbook")
+    }
+    suspend fun orderProduct(user: User, product: Product): String {
+        delay(1000)
+        return "${user.name} / ${product.name}";
+    }
+}
+
+val api = ApiClient()
+
+fun main() = runBlocking {
+    println("Started")
+
+    // TODO: practice coroutine
+
+    println("finished")
 }
